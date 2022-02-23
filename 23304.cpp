aa757c39d397fb37235f89647b3cd72368eade78
송은수 <sksksk705@hanyang.ac.kr>
2022-02-23 16:51:06 +0900

    23304 해결

---
+++ 23304.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <string>
+
+
+using namespace std;
+string s;
+bool check_pal(int start, int end, int depth) {
+	if (start == end)
+		return true;
+	int size = end - start+1;
+	int mid = (start + end) / 2;
+	int left;
+	int right;
+	if (size % 2)
+	{
+		left = mid - 1;
+		right = mid + 1;
+	}
+	else
+	{
+		left = mid;
+		right = mid+1;
+
+	}
+	for (int i = 0; i < size/2; ++i)
+	{
+		if (s[left - i] != s[right + i])
+			return false;
+	}
+	return (check_pal(0, left,depth+1) && check_pal(right, s.size()-1,depth+1));
+}
+
+int main() {
+
+	cin >> s;
+
+	if (check_pal(0,s.size()-1,1))
+		cout << "AKARAKA";
+	else
+		cout << "IPSELENTI";
+}
