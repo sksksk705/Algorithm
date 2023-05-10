747370d423c3c7ae5113070728f5cfb676a7dc90
sksksk705 <80654404+sksksk705@users.noreply.github.com>
2023-05-10 10:21:50 +0900

    [Gold V] Title: 게임을 클리어하자, Time: 180 ms, Memory: 115400 KB -BaekjoonHub

--- 백준/Gold/28017. 게임을 클리어하자/게임을 클리어하자.py
+++ 백준/Gold/28017. 게임을 클리어하자/게임을 클리어하자.py
@@ -1,22 +1,18 @@
 import sys
 input = sys.stdin.readline
-INT_MAX = 2 ** 32
 
 n,m = map(int,input().split())
 
-weapons = []
-for i in range(n):
-    weapons.append(list(map(int,input().split())))
-
-dp = [[INT_MAX]* 501 for _ in range(500)]
-def playGame(time, prevUsed):
-    if time == n:
-        return 0
-    if dp[time][prevUsed] != INT_MAX:
-        return dp[time][prevUsed]
+prev = [0] * m
+for day in range(n):
+    weapons = list(map(int,input().split()))
+    minWeaponIdx = min(range(m),key=prev.__getitem__)
+    prev.sort()
     for i in range(m):
-        if i != prevUsed:
-            dp[time][prevUsed] = min(dp[time][prevUsed], playGame(time+1,i) + weapons[time][i])
-    return dp[time][prevUsed]
-playGame(0,500)
-print(dp[0][500])
+        if i == minWeaponIdx:
+            weapons[i] += prev[1]
+        else:
+            weapons[i] += prev[0]
+    prev = weapons
+
+print(min(prev))
